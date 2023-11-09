16dfe5512c6d143f90ccc72883d58a45452e8ed9
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-11-09 09:31:20 -0600

    added functional getAllPuppies and console log data

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const baseURL = `https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF`;
 
+
+const state = {
+    allPuppies: []
+};
+
+const main = document.querySelector('main');
+main.setAttribute('style', 'display:flex; flex-direction:row; flex-wrap:wrap');
+
+const getAllPuppies = async () => {
+    const response = await fetch(`${baseURL}/players`);
+    const jsonresponse = await response.json();
+    state.allPuppies = jsonresponse.data;
+    console.log(state.allPuppies);
+}
+
+getAllPuppies();
